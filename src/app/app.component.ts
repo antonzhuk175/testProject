2f672bb6013beb6466d1f5556fc9d4ae12dd56a5
antonzhuk175 <juk01anton96@narod.ru>
2018-06-12 16:04:12 +0300

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'YO! YO! YO! :D:D';
-  number = 26;
+  phones: Phone[] = [];
+    companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
+     
+    addPhone(title: string, price: number, company: string){
+        this.phones.push(new Phone(title, price, company));
+    }
+}
+
+export class Phone{
+  constructor(public title: string, 
+              public price: number, 
+              public company: string)
+  { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BoldDirective} from './bold.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BoldDirective
   ],
   imports: [
     BrowserModule,
